7c9b49c5312f0206fb4798ea59001c0c46e5c235
Chris March <556135+chrismarch@users.noreply.github.com>
2022-03-27 11:14:00 -0700

    GPLv3 header

--- Assets/Scripts/SoftBodyPrototype.cs
+++ Assets/Scripts/SoftBodyPrototype.cs
@@ -1,7 +1,21 @@
-﻿// SoftBodyPrototype
-// Author: 
-// Chris March 
-// https://github.com/chrismarch/SoftBodySimulation
+/*
+ * This is the main code file for a prototype simulation of soft body physics <https://github.com/chrismarch/SoftBodySimulation>
+ *
+ * Copyright (C) 2018 Chris March <https://github.com/chrismarch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 using System.Collections;
 using System.Collections.Generic;
